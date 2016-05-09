418b9c70fd67db5de0a3bf72d6338eed8cb30a1e
Simon Wilkins <simonwilkins1970@googlemail.com>
2016-05-09 21:01:31 +0100

    Initial set up of test client

---
+++ src/main/java/a1/CloudConfigClientTest.java
@@ -0,0 +1,21 @@
+package a1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CloudConfigClientTest {
+	
+	@RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClientTest.class, args);
+    }
+
+}
